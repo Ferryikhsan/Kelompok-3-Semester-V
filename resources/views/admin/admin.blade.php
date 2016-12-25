93e835191cb1a268b97d50419f04eded2432dd38
ACER-PC\ACER <Ageng_an@yahoo.com>
2016-12-25 17:21:34 +0800

    Halaman Admin dan Index Admin

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app-admin')
+
+@section('content')
+					
+            <div class="row margin-vert-30">
+                
+                <!-- End Main Text -->
+                <!-- Side Column -->
+                
+                <!-- End Side Column -->
+            </div>
+            <div class="row">
+                <!-- Portfolio -->
+                <!-- Portfolio Item -->
+                
+                
+            </div>
+            <div class="row">
+                
+            </div>
+        </div>
+    </div>
+                
+            </div>
+        </div>
+				</div>
+			</div>
+		</div>
+		<!-- === END CONTENT === -->
+		@endsection
